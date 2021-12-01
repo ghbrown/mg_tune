7b9166c97237e001ae7592893ed5ab3dd67e9e2c
GHBrown <gabriel.h.brown@gmail.com>
2021-12-01 10:14:58 -0600

    Make sandbox.py into failing example

--- sbox/sandbox.py
+++ sbox/sandbox.py
@@ -1,18 +1,17 @@
 
 import numpy as np
-#import pyamg
+import pyamg    #PROBLEM, PYAMG IS BROKEN INSIDE VENV
 import mgtune
 
 n = 100
-#PROBLEM, PYAMG IS BROKEN INSIDE VENV
-#A = pyamg.gallery.poisson((n,n), format='csr')   # matrix
 A = np.random.rand(n,n)
-b = np.ones((A.shape[0]))                            # right hand side
+#A = pyamg.gallery.poisson((n,n), format='csr')   # matrix
+b = np.ones((A.shape[0]))                        # right hand side
 
 A_list = [A]    #put tuning/training set in mgtune's expected form
 b_list = [b]
 
 solver_file = 'sa_solver.py'   #location of solver function file
 
-#what_will_the_output_look_like = mgtune.tune("sa_solver.py",A_list,b_list)
+#mgtune.tune("sa_solver.py",A_list,b_list)
 
